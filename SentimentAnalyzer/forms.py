b0795ad7090aa6babf49a5a16ba8cb4bb88669b6
Tommy Pham <71846316+bobapee@users.noreply.github.com>
2020-10-22 17:33:00 -0500

    Update forms.py

--- SentimentAnalyzer/forms.py
+++ SentimentAnalyzer/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
-class SearchForm(form.forms):
-	tweet_search = forms.CharField(label = 'Type the word or phrase that you want to analyze ', max_length = 50)
+class SearchForm(forms.Form):
+	tweet_search = forms.CharField(max_length = 50, widget = forms.TextInput(attrs={'placeholder' : 'Word or Tweet here...'}))
